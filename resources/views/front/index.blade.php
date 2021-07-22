0b5c5df28c0587bafa1d4b2b01f271c7bccf5d28
AlbertWebs <albertmuhatia@gmail.com>
2021-07-22 16:50:02 +0300

    updateCategories

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -84,128 +84,51 @@
 
         <div class="cat-blocks-container">
             <div class="row">
+                <?php $Categories = DB::table('category')->limit('12')->get(); ?>
+                @foreach($Categories as $Cat)
                 <div class="col-6 col-sm-4 col-lg-2">
-                    <a href="category.html" class="cat-block">
+                    <a href="{{url('/')}}/products/{{$Cat->slung}}" class="cat-block">
                         <figure>
                             <span>
-                                <img src="{{asset('theme/assets/images/demos/demo-13/cats/1.jpg')}}" alt="Category image">
+                                <img style="max-width:131px;" src="{{url('/')}}/uploads/categories/{{$Cat->image}}" alt="{{$Cat->cat}}">
                             </span>
                         </figure>
 
-                        <h3 class="cat-block-title">Computer & Laptop</h3><!-- End .cat-block-title -->
+                        <h3 class="cat-block-title">{{$Cat->cat}}</h3><!-- End .cat-block-title -->
                     </a>
                 </div><!-- End .col-sm-4 col-lg-2 -->
-
-                <div class="col-6 col-sm-4 col-lg-2">
-                    <a href="category.html" class="cat-block">
-                        <figure>
-                            <span>
-                                <img src="{{asset('theme/assets/images/demos/demo-13/cats/2.jpg')}}" alt="Category image">
-                            </span>
-                        </figure>
-
-                        <h3 class="cat-block-title">Lighting</h3><!-- End .cat-block-title -->
-                    </a>
-                </div><!-- End .col-sm-4 col-lg-2 -->
-
-                <div class="col-6 col-sm-4 col-lg-2">
-                    <a href="category.html" class="cat-block">
-                        <figure>
-                            <span>
-                                <img src="{{asset('theme/assets/images/demos/demo-13/cats/3.jpg')}}" alt="Category image">
-                            </span>
-                        </figure>
-
-                        <h3 class="cat-block-title">Smart Phones</h3><!-- End .cat-block-title -->
-                    </a>
-                </div><!-- End .col-sm-4 col-lg-2 -->
-
-                <div class="col-6 col-sm-4 col-lg-2">
-                    <a href="category.html" class="cat-block">
-                        <figure>
-                            <span>
-                                <img src="{{asset('theme/assets/images/demos/demo-13/cats/4.jpg')}}" alt="Category image">
-                            </span>
-                        </figure>
-
-                        <h3 class="cat-block-title">Televisions</h3><!-- End .cat-block-title -->
-                    </a>
-                </div><!-- End .col-sm-4 col-lg-2 -->
-
-                <div class="col-6 col-sm-4 col-lg-2">
-                    <a href="category.html" class="cat-block">
-                        <figure>
-                            <span>
-                                <img src="{{asset('theme/assets/images/demos/demo-13/cats/5.jpg')}}" alt="Category image">
-                            </span>
-                        </figure>
-
-                        <h3 class="cat-block-title">Cooking</h3><!-- End .cat-block-title -->
-                    </a>
-                </div><!-- End .col-sm-4 col-lg-2 -->
-
-                <div class="col-6 col-sm-4 col-lg-2">
-                    <a href="category.html" class="cat-block">
-                        <figure>
-                            <span>
-                                <img src="{{asset('theme/assets/images/demos/demo-13/cats/6.jpg')}}" alt="Category image">
-                            </span>
-                        </figure>
-
-                        <h3 class="cat-block-title">Furniture</h3><!-- End .cat-block-title -->
-                    </a>
-                </div><!-- End .col-sm-4 col-lg-2 -->
+                @endforeach
             </div><!-- End .row -->
         </div><!-- End .cat-blocks-container -->
     </div><!-- End .container -->
 
     <div class="mb-2"></div><!-- End .mb-2 -->
+    <h1 style="font-size:2px; margin:0 auto; color:#fff">Car Audio Shop in Nairobi</h1>
+    <?php $Full = DB::table('product')->where('stock','In Stock')->where('offer','1')->limit('10')->inRandomOrder()->get();  ?>
+    @if($Full->isEmpty())
 
+    @else
     <div class="container">
         <div class="row">
-            <div class="col-sm-6 col-lg-3">
+            @foreach($Full as $full)
+            <div class="col-sm-6 col-lg-4 ">
                 <div class="banner banner-overlay">
-                    <a href="#">
-                        <img src="{{asset('theme/assets/images/demos/demo-13/banners/banner-1.jpg')}}" alt="Banner">
+                    <a href="{{url('/')}}/product/{{$full->slung}}">
+                        <img src="{{url('/')}}/uploads/product/{{$full->offer_banner}}" alt="{{$full->name}}">
                     </a>
 
                     <div class="banner-content">
-                        <h4 class="banner-subtitle text-white"><a href="#">Weekend Sale</a></h4><!-- End .banner-subtitle -->
-                        <h3 class="banner-title text-white"><a href="#">Lighting <br>& Accessories <br><span>25% off</span></a></h3><!-- End .banner-title -->
-                        <a href="#" class="banner-link">Shop Now <i class="icon-long-arrow-right"></i></a>
+                        {{-- <h4 class="banner-subtitle text-white"><a href="#">Weekend Sale</a></h4> --}}
+                        {{-- <h3 class="banner-title text-white"><a href="#">Lighting <br>& Accessories <br><span>25% off</span></a></h3> --}}
+                        {{-- <a href="#" class="banner-link">Shop Now <i class="icon-long-arrow-right"></i></a> --}}
                     </div><!-- End .banner-content -->
                 </div><!-- End .banner -->
             </div><!-- End .col-lg-3 -->
+            @endforeach
 
-            <div class="col-sm-6 col-lg-3 order-lg-last">
-                <div class="banner banner-overlay">
-                    <a href="#">
-                        <img src="{{asset('theme/assets/images/demos/demo-13/banners/banner-3.jpg')}}" alt="Banner">
-                    </a>
-
-                    <div class="banner-content">
-                        <h4 class="banner-subtitle text-white"><a href="#">Smart Offer</a></h4><!-- End .banner-subtitle -->
-                        <h3 class="banner-title text-white"><a href="#">Anniversary <br>Special <br><span>15% off</span></a></h3><!-- End .banner-title -->
-                        <a href="#" class="banner-link">Shop Now <i class="icon-long-arrow-right"></i></a>
-                    </div><!-- End .banner-content -->
-                </div><!-- End .banner -->
-            </div><!-- End .col-lg-3 -->
-
-            <div class="col-lg-6">
-                <div class="banner banner-overlay">
-                    <a href="#">
-                        <img src="{{asset('theme/assets/images/demos/demo-13/banners/banner-2.jpg')}}" alt="Banner">
-                    </a>
-
-                    <div class="banner-content">
-                        <h4 class="banner-subtitle text-white d-none d-sm-block"><a href="#">Amazing Value</a></h4><!-- End .banner-subtitle -->
-                        <h3 class="banner-title text-white"><a href="#">Clothes Trending <br>Spring Collection 2019 <br><span>from $12,99</span></a></h3><!-- End .banner-title -->
-                        <a href="#" class="banner-link">Discover Now <i class="icon-long-arrow-right"></i></a>
-                    </div><!-- End .banner-content -->
-                </div><!-- End .banner -->
-            </div><!-- End .col-lg-6 -->
         </div><!-- End .row -->
     </div><!-- End .container -->
+    @endif
 
     <div class="mb-3"></div><!-- End .mb-3 -->
     
