fb013f1bc82d82cc332eba082833723a35b2f74a
AlbertWebs <albertmuhatia@gmail.com>
2021-08-30 10:52:24 +0300

    Sort * Tooltips

--- resources/views/front/products.blade.php
+++ resources/views/front/products.blade.php
@@ -18,30 +18,7 @@
 
     <div class="page-content">
         <div class="container">
-            <div class="toolbox">
-                <div class="toolbox-left">
-                    <a href="#" class="sidebar-toggler"><i class="icon-bars"></i>Filters</a>
-                </div><!-- End .toolbox-left -->
-
-                <div class="toolbox-center">
-                    <div class="toolbox-info">
-                        Showing <span>Page {{$Products->currentPage()}} | {{$Products->perPage()}} of {{$Products->total()}}</span> Products
-                    </div><!-- End .toolbox-info -->
-                </div><!-- End .toolbox-center -->
-
-                <div class="toolbox-right">
-                    <div class="toolbox-sort">
-                        <label for="sortby">Sort by:</label>
-                        <div class="select-custom">
-                            <select name="sortby" id="sortby" class="form-control">
-                                <option value="popularity" selected="selected"> Lowest to Highest </option>
-                                <option value="rating">Highest to Lowest </option>
-                                <option value="date">Date</option>
-                            </select>
-                        </div>
-                    </div><!-- End .toolbox-sort -->
-                </div><!-- End .toolbox-right -->
-            </div><!-- End .toolbox -->
+            @include('front.tool')
 
             <div class="products">
                 <div class="row">
